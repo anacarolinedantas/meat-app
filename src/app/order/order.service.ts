60b1660b9f7b9d946f6b38a1fe1844cb6987b799
Carol Dantas <caroldantas@ufrn.edu.br>
2020-11-10 12:36:38 -0300

    Finaliza a compra (HTTP POST)

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from "@angular/core";
 import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
 import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { Observable } from "rxjs/Observable";
+import { Order, OrderItem } from "./order.model";
+import { Http, Headers, RequestOptions } from "@angular/http";
+import 'rxjs/add/operator/map'
+import { MEAT_API } from "app/app.api";
 
 @Injectable()
 export class OrderService {
 
-    constructor(private cartService: ShoppingCartService) {}
+    constructor(private cartService: ShoppingCartService, private http: Http) {}
 
     itemsValue(): number {
         return this.cartService.total()
@@ -26,4 +31,19 @@ export class OrderService {
     remove(item: CartItem) {
         this.cartService.removeItem(item)
     }
+
+    clear() {
+        this.cartService.clear()
+    }
+
+    checkOrder(order: Order): Observable<string> {
+        const headers = new Headers()
+        headers.append('Content-Type', 'application/json')
+        return this.http.post(`${MEAT_API}/orders`,
+                              JSON.stringify(order),
+                              new RequestOptions({headers: headers}))
+                        .map(response => response.json())
+    }
+
+    
 }
