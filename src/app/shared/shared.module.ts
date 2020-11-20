a389b5274cd7bbf45911880e0699ff436c7c5d69
Carol Dantas <caroldantas@ufrn.edu.br>
2020-11-20 20:01:03 -0300

    Cria o serviço de login

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -11,6 +11,7 @@ import { OrderService } from "app/order/order.service";
 import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
 import { RestaurantsService } from "app/restaurants/restaurants.service";
 import { NotificationService } from './messages/notification.service';
+import { LoginService } from 'app/security/login/login.service';
 
 
 @NgModule({
@@ -22,7 +23,11 @@ export class SharedModule {
     static forRoot(): ModuleWithProviders {
         return {
             ngModule: SharedModule,
-            providers: [ShoppingCartService, RestaurantsService, OrderService, NotificationService]
+            providers: [ShoppingCartService, 
+                        RestaurantsService, 
+                        OrderService, 
+                        NotificationService,
+                        LoginService]
         }
     }
 }
