0d5fd316c2f215a9a134cac301b540b2f3120a85
Carol Dantas <caroldantas@ufrn.edu.br>
2020-11-20 20:40:59 -0300

    Realiza compra com autenticação

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NotificationService } from 'app/shared/messages/notification.service';
 import { LoginService } from './login.service';
 
 
@@ -13,7 +14,8 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup
 
   constructor(private fb: FormBuilder, 
-              private loginService: LoginService) { }
+              private loginService: LoginService,
+              private notificationService: NotificationService) { }
 
   ngOnInit() {
     this.loginForm = this.fb.group({
@@ -24,7 +26,8 @@ export class LoginComponent implements OnInit {
 
   login() {
     this.loginService.login(this.loginForm.value.email, this.loginForm.value.password)
-                     .subscribe(user => console.log(user))
+                     .subscribe(user => this.notificationService.notify(`Bem vindo, ${user.name}`),
+                                response => this.notificationService.notify(response.error.message)) //HttpErrorResponse
   }
 
 }
