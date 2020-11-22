421920d077bd284a89b374e242ac911c50be4967
Carol Dantas <caroldantas@ufrn.edu.br>
2020-11-22 18:53:17 -0300

    Utiliza NgZone para Error Handler

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,4 +1,4 @@
-import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { ErrorHandler, Injectable, Injector, NgZone } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http'
 import { NotificationService } from './shared/messages/notification.service';
 import { LoginService } from './security/login/login.service';
@@ -7,24 +7,26 @@ import 'rxjs/add/observable/throw';
 @Injectable()
 export class AplicationErrorHandler extends ErrorHandler {
 
-    constructor(private ns: NotificationService, private injector: Injector) {
+    constructor(private ns: NotificationService, private injector: Injector, private zone: NgZone) {
         super()
     }
 
     handleError (errorResonse: HttpErrorResponse | any) {
         if(errorResonse instanceof HttpErrorResponse) {
             const message = errorResonse.error.message
-            switch(errorResonse.status) {
-                case 401:
-                    this.injector.get(LoginService).handleLogin()
-                    break;
-                case 403:
-                    this.ns.notify(message || 'Não autorizado')
-                    break;
-                case 404:
-                    this.ns.notify(message || 'Recurso não encontrado. Verifique o console para mais detalhes.')
-                    break;
-            }
+            this.zone.run(() => {
+                switch(errorResonse.status) {
+                    case 401:
+                        this.injector.get(LoginService).handleLogin()
+                        break;
+                    case 403:
+                        this.ns.notify(message || 'Não autorizado')
+                        break;
+                    case 404:
+                        this.ns.notify(message || 'Recurso não encontrado. Verifique o console para mais detalhes.')
+                        break;
+                }
+            })
         }
         super.handleError(errorResonse)
     }
